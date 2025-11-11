eac82c1ab3375dca65ef6e83f6a1ff457b2c8072
Roy Murphy <roycoins@gmail.com>
2025-11-11 03:09:14 +0000

    Update encode.rs

--- src/encode.rs
+++ src/encode.rs
@@ -7,10 +7,7 @@
 use std::arch::aarch64::{vgetq_lane_u64, vld1q_u64, vsetq_lane_u64, vst1q_u64};
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::{
-    _mm256_extract_epi64,
-    _mm256_insert_epi64,
-    _mm256_loadu_si256,
-    _mm256_storeu_si256,
+    _mm256_extract_epi64, _mm256_insert_epi64, _mm256_loadu_si256, _mm256_storeu_si256,
 };
 
 const VAL_TO_DIGIT: [u8; 58] = [
